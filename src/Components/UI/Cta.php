71717bfc2469cc4aba6f229e3bd80b84f198092f
Soheil Nazari <kontakt@soheilnazari.de>
2024-08-24 23:29:07 +0200

    add address form

---
+++ src/Components/UI/Cta.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Components\UI;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent(template: '@Components/UI/Cta.twig')]
+class Cta
+{
+}
---
+++ src/Components/UI/Input.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Components\UI;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent(template: '@Components/UI/Input.twig')]
+class Input
+{
+}
