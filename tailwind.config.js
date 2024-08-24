efa551e0710cf03826896ef104ac26fd683964a0
Soheil Nazari <kontakt@soheilnazari.de>
2024-08-24 22:48:50 +0200

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/Components/**/*.twig",
+    "./src/Layouts/**/*.twig",
+    "./src/Pages/**/*.twig",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
